cf105c2e84db48603e72c1a7833754bea6b7a6e8
MedmeFord <82370059+MedmeFord@users.noreply.github.com>
2022-12-23 18:54:59 +0300

    ReadyWriteToUser

---
+++ src/User.java
@@ -0,0 +1,31 @@
+import java.util.UUID;
+
+public class User {
+    private final java.util.UUID UUID = java.util.UUID.randomUUID(); //
+    private String firstName;
+    private String middleName;
+    private String secondName;
+    private String login;
+    private String password;
+
+    public User(String firstName, String middleName
+            , String secondName, String login, String password) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.secondName = secondName;
+        this.login = login;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User: " +
+                "UUID=" + UUID + "\'\n" +
+                "FirstName='" + firstName + "\'\n"+
+                "MiddleName='" + middleName +"\'\n" +
+                "SecondName='" + secondName + "\'\n" +
+                "Login='" + login + "\'\n" +
+                "Password='" + password + "\'\n" +
+                "\n";
+    }
+}
---
+++ src/WriteToFile.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.nio.file.OpenOption;
+import java.util.ArrayList;
+
+public class WriteToFile {
+    private String path;
+    private final String separator = File.separator;
+
+    WriteToFile() {
+        this.path = "." + separator + "Datafile";
+    }
+
+    WriteToFile(String path) {
+
+        try {
+            if (path == null) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException err) {
+            System.out.printf("Path == null ");
+            System.exit(0);
+        }
+        this.path = path;
+    }
+
+    public void writeToFile(ArrayList<User> users) {
+        try {
+            FileOutputStream outputStream = new FileOutputStream(path);
+            for (int i = 0; i < users.size(); i++) {
+                String str = users.get(i).toString();
+                byte[] strToBytes = str.getBytes();
+                outputStream.write(strToBytes);
+            }
+            outputStream.close();
+        } catch (IOException err) {
+            err.printStackTrace();
+        }
+    }
+
+    public void printPath() {
+        System.out.println(this.path);
+    }
+}
+
+class Test {
+    public static void main(String[] argv) {
+        WriteToFile writeFile = new WriteToFile();
+
+        User user1 = new User("Ivan", "Ivanov", "Ivanovich", "Medme", "pass");
+        User user2 = new User("Ivan1", "Ivanov1", "Ivanovich1", "Medme1", "pass1");
+
+        ArrayList<User> usersArr = new ArrayList<>();
+
+        usersArr.add(user1);
+        usersArr.add(user2);
+
+        writeFile.printPath();
+        writeFile.writeToFile(usersArr);
+    }
+}
